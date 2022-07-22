c7d5d9f57584239675161dee60568a6ac7762df4
Mert Koca <koca.mrt0@gmail.com>
2022-07-22 10:56:09 +0300

    fix spring not routing to react index after login

--- Server/src/main/java/AgileExpress/Server/Controllers/IndexController.java
+++ Server/src/main/java/AgileExpress/Server/Controllers/IndexController.java
@@ -2,14 +2,15 @@ package AgileExpress.Server.Controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HomeController {
+public class IndexController {
 
-    @RequestMapping(value = {"/", "/index.html"})
+    @GetMapping("")
     public ModelAndView home() {
-        return new ModelAndView("../static/index");
+        return new ModelAndView("index");
     }
 }
