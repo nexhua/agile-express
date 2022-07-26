a118fa0bb4354de51934a1ad73fcebd83b9115dd
Mert Koca <koca.mrt0@gmail.com>
2022-07-26 09:59:43 +0300

    code cleanup

--- Server/src/main/java/AgileExpress/Server/Repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package AgileExpress.Server.Repositories;
-
-import AgileExpress.Server.Entities.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-    Product findByName(String name);
-}
--- Server/src/main/java/AgileExpress/Server/ServerApplication.java
+++ Server/src/main/java/AgileExpress/Server/ServerApplication.java
@@ -1,28 +1,14 @@
 package AgileExpress.Server;
 
-import AgileExpress.Server.Entities.Product;
-import AgileExpress.Server.Repositories.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackageClasses=ProductRepository.class)
-public class ServerApplication implements CommandLineRunner {
-
-	@Autowired
-	private ProductRepository repository;
+@EnableMongoRepositories
+public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		repository.deleteAll();
-
-		repository.save(new Product("Tooh brush","brushing mechanics"));
 	}
 }
